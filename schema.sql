3f2bfca2f1c9678b0a478d2baa5b9d32c31fa596
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-06-17 23:58:49 -0700

    Book-library

---
+++ schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (id SERIAL PRIMARY KEY, 
+title VARCHAR(255), 
+authors VARCHAR(255), 
+description TEXT, thumbnail VARCHAR(255), 
+isbn VARCHAR(255), bookshelf VARCHAR(255));
